3aac61fffaab80440fa084b9a97036790fd81f6a
Pascal <pascalheilmann95@gmail.com>
2024-11-22 13:27:02 +1000

    git init

---
+++ README.md
@@ -0,0 +1,24 @@
+# Mathematical Functions Project
+
+This repository contains Python scripts with mathematical functions. The goal of this project is to demonstrate my Python skills through practical implementations of common mathematical algorithms.
+
+## Features
+
+- **Compound Interest Calculator**: A function to calculate compound interest for a savings plan over a specified number of years.
+- Additional mathematical functions will be added in the future.
+
+## Requirements
+
+- Python 3.x
+- No additional libraries required for the current version.
+
+## Usage
+
+1. First, you need to download the repository to your local machine. You can either clone it using Git or download it as a ZIP file.
+2. After downloading the repository, navigate to the folder where the repository is located.
+3. You can then run the Python scripts using the Python interpreter. For example:
+```bash
+python compound_interest.py
+```
+## License
+This project is licensed under the MIT [LICENSE](https://github.com/Pacalll/math/blob/LICENSE) - see the LICENSE file for details.
---
+++ compound_interest.py
@@ -0,0 +1,29 @@
+def compound_interest(annual_saving_rate, interest, years):
+    """
+        Calculates and prints the compound interest for a savings plan over a specified number of years.
+
+        Parameters:
+        annual_saving_rate (float or int): The amount of money saved annually.
+        interest (float or int): The annual interest rate (in percentage).
+        years (int): The number of years for which the interest is calculated.
+
+        Returns:
+        None: The function prints the results directly.
+
+        Example:
+        1. Year Total Sum:  3,210.00     Total Savings: 3,000.00     Interest: 210.00
+        2. Year Total Sum:  6,586.70     Total Savings: 6,000.00     Interest: 586.70
+        ...
+    """
+    if not isinstance(years, int) or  not isinstance(interest, (float, int)) or not isinstance(annual_saving_rate, (int, float)):
+        print(f"Error: No correct init of variables")
+        return -1
+    total_sum = 0
+    for x in range(1,years+1):
+        total_sum = (total_sum + annual_saving_rate) * ((interest + 100) / 100)
+        total_savings = annual_saving_rate * x
+        interest_sum = total_sum - total_savings
+        print(f"{x:>{len(str(years))}}.Year Total Sum: {total_sum:<{len(str(years))+20},.2f} Total Savings: {total_savings:<{len(str(years))+20},.2f} Interest: {interest_sum:<{len(str(years))},.2f}")
+
+if __name__ == "__main__":
+    compound_interest(1200, 7, 18)
